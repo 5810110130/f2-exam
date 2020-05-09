6b461d58b1de9e7085132a2144c559d5fa6abf56
5810110130 <39266565+ThanapornSaechin@users.noreply.github.com>
2020-05-09 10:48:44 +0700

    Create ReadFile.java

---
+++ ReadFile.java
@@ -0,0 +1,8 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class ReadFile {
+  public static void main(String[] args) {
+      
+}
