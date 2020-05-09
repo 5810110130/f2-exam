125bdc01289a13a02953b0b4ded14de97e03220e
5810110130 <39266565+ThanapornSaechin@users.noreply.github.com>
2020-05-09 14:59:12 +0700

    Update Class
    - cuboid
    - cylinder
    - sphere

---
+++ Sphere.java
@@ -0,0 +1,24 @@
+public class Sphere extends Shape3D {
+    private double radius;
+
+    public Sphere(String name, double radius){
+        super(name);
+        this.radius = radius;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return Math.PI * radius * radius * radius * 4;
+    }
+
+    public double getVolume(){
+		return Math.PI * radius * radius * radius * (4/3); 
+    }
+}
