d9f7d4cfc433c1e503ebc4004da23f0597723436
5810110130 <39266565+ThanapornSaechin@users.noreply.github.com>
2020-05-09 11:04:02 +0700

    Update ReadFile.java
    test read file

--- ReadFile.java
+++ ReadFile.java
@@ -4,5 +4,17 @@ import java.util.Scanner; // Import the Scanner class to read text files
 
 public class ReadFile {
   public static void main(String[] args) {
-      
+    try {
+      File myObj = new File("test.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
 }
