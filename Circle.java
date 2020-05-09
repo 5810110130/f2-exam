a3b24bb9d7f81408339eeec67242d58ba0751eeb
5810110130 <39266565+ThanapornSaechin@users.noreply.github.com>
2020-05-09 14:14:48 +0700

    update circle & rec
    extends shape2d

--- Circle.java
+++ Circle.java
@@ -1,3 +1,25 @@
-public class Circle {
+public class Circle extends Shape2D {
+    private double radius;
+
+	public Circle(String name, double radius){
+		super(name);
+		this.radius = radius;
+	}
+
+	public double getRadius(){
+		return radius;
+	}
+
+	public void setRadius(double radius){
+		this.radius = radius;
+	}
+
+	public double getArea(){
+		return Math.PI * radius * radius;
+	}
+
+	public void zoom(double factor){
+		radius = radius * factor;
+	}
     
 }
