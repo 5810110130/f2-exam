7f7f8aa6a3f3f8f29eb2a374afe9cce1ad94cc4a
5810110130 <39266565+ThanapornSaechin@users.noreply.github.com>
2020-05-09 14:45:31 +0700

    Update Main.java
    - Build Method ReadFile

--- Main.java
+++ Main.java
@@ -4,27 +4,25 @@ import java.util.Scanner; // Import the Scanner class to read text files
 public class Main {
 
     public Main(String path){
-        String[] data = this.readFile(path);
-        for(String d : data)
-            System.out.println(d);
+        String data = this.readFile(path);
+        System.out.print(data);
     }
     
-
-    private String[] readFile(String path) { // method for readfile & split data
+    private String readFile(String path) { // method for readfile & split data
+        String data = "";
 
         try {
-        String inputfile = "C:/xampp/htdocs/f2-exam/test.txt";
-        File file = new File(inputfile);
-        Scanner s = new Scanner(file);
-        while (s.hasNextLine()) {
-          String data = s.nextLine();
-          System.out.println(data);  
-        }
-         s.close();
+            File myObj = new File(path);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()){
+                data += myReader.nextLine();
+            }
+            myReader.close();
         } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
+            System.out.println("File Not Found!");
             e.printStackTrace();
         }
+        return data;
 
-    }   
+        //method for split
 }
